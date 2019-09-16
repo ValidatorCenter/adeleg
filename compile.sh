863c6ddbdcee6c17c37f78c4f81ce73e6c9c4d48
Web <web@k0d.su>
2019-09-16 14:19:35 +0200

    fix sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 GOOS=windows GOARCH=amd64 go build -ldflags "-s" -o adlg_win64.exe deleg.go
 GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o adlg_lin64 deleg.go
 GOOS=darwin GOARCH=amd64 go build -ldflags "-s" -o adlg_mac64 deleg.go
