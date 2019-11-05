1bb05ed27250a0fd0203a925a5aeb1cf4cfa2524
Dimitry <dkodnik@yahoo.com>
2019-11-05 15:32:20 +0300

    v1.2.0 (#5)
    * multirule
    
    * doc ext
    
    * fdtab
    
    * check 100%
    
    * cfg

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rm ./adlg_win64.exe
+rm ./adlg_lin64
+rm ./adlg_mac64
+
 GOOS=windows GOARCH=amd64 go build -ldflags "-s" -o adlg_win64.exe deleg.go
 GOOS=linux GOARCH=amd64 go build -ldflags "-s" -o adlg_lin64 deleg.go
 GOOS=darwin GOARCH=amd64 go build -ldflags "-s" -o adlg_mac64 deleg.go
